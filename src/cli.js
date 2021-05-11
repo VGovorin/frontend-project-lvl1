14d8c6a39812ef85dae3b526601577d6dac7969c
vgovorin <vvgovorin@gmail.com>
2021-05-11 19:28:45 +0800

    Add eslint, eslint-config-airbnb-base, eslint-plugin-import
    Added .eslintrc.yaml. Fix src/cli.js, bin/brain-games.js. Update Makefile

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 export default () => {
-	const greetingText = 'Wlcome to the Brain Games!\nMay I have your name? ';
-	const name = readlineSync.question(greetingText);
-	console.log(`Hello, ${name}!`);
+  const greetingText = 'Wlcome to the Brain Games!\nMay I have your name? ';
+  const name = readlineSync.question(greetingText);
+  console.log(`Hello, ${name}!`);
 };
